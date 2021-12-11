240e35cbd8485f64ea527bb101829b8cc95f8eb8
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-11 16:02:44 -0300

    tests: ensure File throws an Error when csv file has more than three items

--- 1-tests/class-01/index.test.js
+++ 1-tests/class-01/index.test.js
@@ -1,10 +1,18 @@
 const { error } = require('./src/constants')
 const File = require('./src/file')
 const { rejects } = require('assert')
-
+;
 (async () => {
-  const filePath = './mocks/empty-file-invalid.csv'
-  const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
-  const result = File.csvToJson(filePath)
-  await rejects(result, rejection)
+  {
+    const filePath = './mocks/empty-file-invalid.csv'
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+    const result = File.csvToJson(filePath)
+    await rejects(result, rejection)
+  }
+  {
+    const filePath = './mocks/four-items-file.csv'
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+    const result = File.csvToJson(filePath)
+    await rejects(result, rejection)
+  }
 })()
