d18da94477202ae7788f9a5b939a10adbbed08f8
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-14 17:56:16 -0300

    feat: add carCategory

--- 1-tests/class-05-project/part-1/seed/index.js
+++ 1-tests/class-05-project/part-1/seed/index.js
@@ -1,6 +1,16 @@
 const faker = require('faker')
 
-console.log({
+const Car = require('../src/entitites/car')
+const CarCategory = require('../src/entitites/car-category')
+const Customer = require('../src/entitites/customer')
+const { join } = require('path')
+const seederBaseFolder = join(__dirname, '../', 'database')
+
+const carCategory = new CarCategory({
   id: faker.datatype.uuid(),
-  name: faker.name.firstName()
+  name: faker.vehicle.type(),
+  carIds: [],
+  price: faker.finance.amount(20, 200)
 })
+
+console.log(carCategory)
