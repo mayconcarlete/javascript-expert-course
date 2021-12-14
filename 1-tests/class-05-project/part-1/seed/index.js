ffd429d0189a1045a8536caaa72c989a75e2e928
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-14 20:43:13 -0300

    feat: add seeds created by faker

--- 1-tests/class-05-project/part-1/seed/index.js
+++ 1-tests/class-05-project/part-1/seed/index.js
@@ -5,6 +5,8 @@ const CarCategory = require('../src/entitites/car-category')
 const Customer = require('../src/entitites/customer')
 const { join } = require('path')
 const seederBaseFolder = join(__dirname, '../', 'database')
+const ITEMS_AMOUNT = 3
+const { writeFile } = require('fs/promises')
 
 const carCategory = new CarCategory({
   id: faker.datatype.uuid(),
@@ -13,4 +15,34 @@ const carCategory = new CarCategory({
   price: faker.finance.amount(20, 200)
 })
 
-console.log(carCategory)
+const cars = []
+const customers = []
+for(let i = 0; i < ITEMS_AMOUNT; i++){
+  const car = new Car({
+    id: faker.datatype.uuid(),
+    name: faker.vehicle.vehicle(),
+    releaseYear: faker.date.past().getFullYear(),
+    available: true,
+    gasAvailable: true,
+  })
+  carCategory.carIds.push(car.id)
+  cars.push(car)
+
+  const customer = new Customer({
+    id: faker.datatype.uuid(),
+    name: faker.name.findName(),
+    age: faker.datatype.number({min: 18, max:50})
+  })
+  customers.push(customer)
+}
+const write = (filename, data) => writeFile(join(seederBaseFolder, filename), JSON.stringify(data))
+;(
+  async() => {
+    await write('cars.json', cars)
+    await write('customers.json', customers)
+    await write('car-categories.json', [carCategory])
+    console.log('cars:', cars)
+    console.log('categories:', carCategory)
+    console.log('customers:', customers)
+  }
+)()
