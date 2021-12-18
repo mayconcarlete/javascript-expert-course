ce124ab63ff38ff840d88ea2c8e9d3c2323638b2
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-18 11:28:13 -0300

    feat: part-2 of project class

--- 1-tests/class-01-mocks/index.test.js
+++ 1-tests/class-01-mocks/index.test.js
@@ -1,43 +1,38 @@
-const { error } = require('./src/constants')
-const File = require('./src/file')
-const { rejects, deepStrictEqual } = require('assert')
-;
-(async () => {
-  {
-    const filePath = './mocks/empty-file-invalid.csv'
-    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
-    const result = File.csvToJson(filePath)
-    await rejects(result, rejection)
-  }
-  {
-    const filePath = './mocks/four-items-file.csv'
-    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
-    const result = File.csvToJson(filePath)
-    await rejects(result, rejection)
-  }
-  {
-    const filePath = './mocks/three-items-file.csv'
-    const result = await File.csvToJson(filePath)
-    const expected = [
-      {
-        "id": 1,
-        "name": "Maycon Carlete",
-        "profession": "Javascript",
-        "birthDate": 1990
-      },
-      {
-        "id": 2,
-        "name": "Erick Wendel",
-        "profession": "python",
-        "birthDate": 2001
-      },
-      {
-        "id": 3,
-        "name": "Carolina Lima",
-        "profession": "C#",
-        "birthDate": 1994
-      }
-    ]
-    deepStrictEqual(JSON.stringify(result), JSON.stringify(expected))
-  }
-})()
+// const { error } = require('./src/constants')
+// const File = require('./src/file')
+// const { rejects, deepStrictEqual } = require('assert')
+// ;
+// (async () => {
+
+//   {
+//     const filePath = './mocks/four-items-file.csv'
+//     const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+//     const result = File.csvToJson(filePath)
+//     await rejects(result, rejection)
+//   }
+//   {
+//     const filePath = './mocks/three-items-file.csv'
+//     const result = await File.csvToJson(filePath)
+//     const expected = [
+//       {
+//         "id": 1,
+//         "name": "Maycon Carlete",
+//         "profession": "Javascript",
+//         "birthDate": 1990
+//       },
+//       {
+//         "id": 2,
+//         "name": "Erick Wendel",
+//         "profession": "python",
+//         "birthDate": 2001
+//       },
+//       {
+//         "id": 3,
+//         "name": "Carolina Lima",
+//         "profession": "C#",
+//         "birthDate": 1994
+//       }
+//     ]
+//     deepStrictEqual(JSON.stringify(result), JSON.stringify(expected))
+//   }
+// })()
--- 1-tests/class-05-project/part-2/tests/unit-tests/car-service.js
+++ 1-tests/class-05-project/part-2/tests/unit-tests/car-service.js
@@ -1,1 +1,62 @@
-const sinon = require('sinons')
+const sinon = require('sinon')
+const { describe, it, before, beforeEach, afterEach } = require('mocha')
+const CarService = require('./../../services/car-service')
+const {join} = require('path')
+const carsDatabase = join(__dirname, './../../database', 'cars.json')
+const { expect } = require('chai')
+const mocks = {
+  validCarCateogry: require('./../mocks/valid-category.json'),
+  validCar: require('./../mocks/valid-car.json'),
+  validCustomer: require('./../mocks/valid-customer.json')
+}
+
+describe('CarService Suit Tests', () =>{
+  let carService = {}
+  let sandBox = {}
+  before(()=> {
+    carService = new CarService({
+      cars: carsDatabase
+    })
+  })
+  beforeEach(() => {
+    sandBox = sinon.createSandbox()
+  })
+  afterEach(() => {
+    sandBox.restore()
+  })
+  it('should retrieve a random position from an array', () => {
+    const data = [0,1,2,3,4]
+    const result = carService.getRandomPositionFromArray(data)
+    expect(result).to.be.lte(data.length).and.be.gte(0)
+  })
+  it('should choose the first id from carIds in carCategory', () => {
+    const carCategory = mocks.validCarCateogry
+    const carIndex = 0
+    sandBox.stub(
+      carService,
+      carService.getRandomPositionFromArray.name
+    ).returns(carIndex)
+    const result = carService.chooseRandomCar(carCategory)
+    const expected = carCategory.carIds[carIndex]
+    expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok
+    expect(result).to.be.equal(expected)
+  })
+  it('given a carCategory it should return an available car', async() => {
+    const car = mocks.validCar
+    const carCategory = Object.create(mocks.validCarCateogry)
+    carCategory.carIds = [ car.id ]
+    sandBox.stub(
+      carService.carRepository,
+      carService.carRepository.find.name,
+    ).resolves(car)
+    sandBox.spy(
+      carService,
+      carService.chooseRandomCar.name
+    )
+    const result = await carService.getAvailableCar(carCategory)
+    const expected = car
+    expect(carService.chooseRandomCar.calledOnce).to.be.ok
+    expect(carService.carRepository.find.calledWithExactly(car.id))
+    expect(result).to.be.deep.equal(expected)
+  })
+})
