6e4db8cd17b12085c23c02581b303fd1cab95233
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-19 18:24:43 -0300

    refactor: improve legibility

--- 1-tests/class-06-desafio/src/index.js
+++ 1-tests/class-06-desafio/src/index.js
@@ -1,6 +1,8 @@
 const http = require('http')
-const {join} = require('path')
+const { join } = require('path')
+
 const CarService = require('./../../class-05-project/part-3/services/car-service')
+
 const carPath = join(__dirname, './../../class-05-project/part-3/database', 'cars.json')
 const customerPath = join(__dirname, './../../class-05-project/part-3/database', 'customers.json')
 const carCategoryPath = join(__dirname, './../../class-05-project/part-3/database', 'car-categories.json')
@@ -14,10 +16,6 @@ const mocks = {
 const routes = {
   '/rent:post': async (request, response) => {
     const carService = new CarService({cars: carPath})
-    console.log(mocks.cars)
-    console.log(mocks.carCategory)
-    console.log(mocks.customers)
-
     const result = await carService.rent(mocks.customers[0], mocks.carCategory[0], 5)
     response.write(JSON.stringify({car: result}))
     return response.end()
@@ -28,7 +26,6 @@ const handler = async(request, response) => {
   const {url, method} = request
   const routePath = `${url}:${method}`.toLocaleLowerCase()
   const chosen = routes[routePath]
-
   response.writeHead(200, {
     'Content-Type': 'application/json'
   })
