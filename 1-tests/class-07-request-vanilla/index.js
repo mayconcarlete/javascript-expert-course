63ebe117f7f224f2e2b734420fee348b2e03f5d7
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-20 21:26:42 -0300

    feat: using https using promise

--- 1-tests/class-07-request-vanilla/index.js
+++ 1-tests/class-07-request-vanilla/index.js
@@ -3,9 +3,22 @@ const https = require('https')
 const url = 'https://swapi.dev/api/planets/1/'
 const url2 = 'https://swapi.dev/api/people/12ss/'
 ;
-(async() => {
-  const response = await https.get(url2, response => {
-    response.on('data', data => console.log(JSON.parse(data)))
-    response.on('error', error => console.log(error))
+// (async() => {
+//   const response = await https.get(url2, response => {
+//     response.on('data', data => console.log(JSON.parse(data)))
+//     response.on('error', error => console.log(error))
+//   })
+// })()
+
+const makeRequest = () => {
+  return new Promise((resolve, reject) => {
+    https.get(url2, response => {
+      response.on('data', data => resolve(JSON.parse(data)))
+      response.on('error', error => resolve(JSON.parse(error)))
+    })
   })
-})()
+}
+
+makeRequest()
+  .then(console.log)
+  .catch(console.log)
