bf8715bbdf6a21eb662fc29811d115a742c26b6b
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-21 21:12:55 -0300

    feat: valueOf

--- 2-data-types/index.js
+++ 2-data-types/index.js
@@ -11,3 +11,28 @@ let obj2 = obj1
 obj1.a +=1
 
 console.assert(obj2.a === 2, 'Error: object is different')
+
+
+console.assert(String(123) === '123', 'Error: cant convert type')
+
+const item = {
+  name: 'Maycon',
+  age: 31,
+  toString(){
+    return `Name: ${this.name}, Age: ${this.age}`
+  }
+}
+console.log('item: ', item + 0)
+
+const item2 = {
+  name: 'Maycon',
+  age: 31,
+  toString(){
+    return `Name: ${this.name}, Age: ${this.age}`
+  },
+  valueOf(){
+    return 007
+  }
+}
+console.log('item: ', item2 + 0)
+console.log('item: ', ''.concat(item2) + 0)
