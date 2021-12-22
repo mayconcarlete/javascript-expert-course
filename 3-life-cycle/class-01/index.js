aa2a791267f4284b6bcc9d13f92cb77c9634b6e3
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-22 19:57:10 -0300

    feat: add class without bind

--- 3-life-cycle/class-01/index.js
+++ 3-life-cycle/class-01/index.js
@@ -2,6 +2,20 @@
 
 const { watch, promises: { readFile } } = require('fs')
 
-watch(__filename, async(event, filename) => {
-  console.log((await readFile(filename)).toString())
-})
+
+class File {
+  watch(event, filename){
+    this.showContent(filename)
+  }
+  async showContent(){
+    console.log((await readFile(filename)).toString())
+  }
+}
+
+// watch(__filename, async(event, filename) => {
+//   console.log((await readFile(filename)).toString())
+// })
+
+const file =  new File()
+
+watch(__filename, file.watch)
