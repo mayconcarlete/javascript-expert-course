3979b30af8fc8b586f2aefc18abde9634eb99a5d
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-23 21:06:45 -0300

    feat: a different way to mock things in JS

--- 3-life-cycle/class-01/index.js
+++ 3-life-cycle/class-01/index.js
@@ -8,3 +8,11 @@ class Person {
 
 const person = new Person()
 person.sayHello('Maycon')
+
+function sayHi(name){
+  console.log(`Hi ${name}`)
+}
+
+person.sayHello = sayHi
+
+person.sayHello('Maycon')
