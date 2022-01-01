e382835e7cd0bfd2f48280d309af6ad38f4437e9
Maycon Carlete <maycon.carlete@gmail.com>
2022-01-01 16:54:00 -0300

    feat: nodejs streams exercise 2

--- 6-streams/index.js
+++ 6-streams/index.js
@@ -1,30 +1,30 @@
 // primeiro
 
-const stdin = process.stdin
-  .on('data', msg => console.log('Dados de entrada: ', msg.toString()))
+// const stdin = process.stdin
+//   .on('data', msg => console.log('Dados de entrada: ', msg.toString()))
 
 
-const stdout = process.stdout
-  .on('data', msg => console.log('data saida: ', msg.toString()))
+// const stdout = process.stdout
+//   .on('data', msg => console.log('data saida: ', msg.toString()))
 
-stdin.pipe(stdout)
+// stdin.pipe(stdout)
 
-// const http = require( 'http')
-// const { readFileSync, createReadStream } = require( 'fs')
-// const {join} = require( 'path')
-// // node -e "process.stdout.write(crypto.randomBytes(1e9))" > big.file
+const http = require( 'http')
+const { readFileSync, createReadStream } = require( 'fs')
+const {join} = require( 'path')
+// node -e "process.stdout.write(crypto.randomBytes(1e9))" > big.file
 
-// const pathFile = join(__dirname, 'big.file')
+const pathFile = join(__dirname, 'big.file')
 
-// // segundo
+// segundo
 
-// // http.createServer((req, res) => {
-// //   // const file = readFileSync(pathFile)
-// //   // res.write(file)
-// //   // res.end()
-// //   createReadStream(pathFile)
-// //   .pipe(res)
-// // }).listen(3000, () => console.log('Running at 3000'))
+http.createServer((req, res) => {
+  // const file = readFileSync(pathFile)
+  // res.write(file)
+  // res.end()
+  createReadStream(pathFile)
+  .pipe(res)
+}).listen(3000, () => console.log('Running at 3000'))
 
 
 
