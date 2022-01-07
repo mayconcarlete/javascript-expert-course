cdd406512b43b948bc943241c7b20c78278fada5
Maycon Carlete <maycon.carlete@gmail.com>
2022-01-06 22:56:23 -0300

    feat: add rekognition exercise

--- 10-aws-expert/02-lambda-serverless/image-analysis/handler.js
+++ 10-aws-expert/02-lambda-serverless/image-analysis/handler.js
@@ -1,8 +1,21 @@
 "use strict";
-
+const {promises: { readFile } } = require('fs')
 class Handler {
+  constructor({rekoSvc}){
+    this.rekoSvc = rekoSvc
+  }
+  async detectImages(buffer){
+    const result = await this.rekoSvc.detectLabels({
+      Image:{
+        Bytes: buffer
+      }
+    }).promise()
+    console.log(result)
+  }
   async main(event){
     try{
+      const imgBuffer = await readFile('dog.jpg')
+      await this.detectImages(imgBuffer)
       return {
         statusCode: 200,
         body: 'ok'
@@ -19,7 +32,7 @@ class Handler {
 const aws = require('aws-sdk')
 const reko = new aws.Rekognition()
 const handler =  new Handler({
-  rekoSvc = reko
+  rekoSvc:reko
 })
 
 module.exports.main = handler.main.bind(handler)
