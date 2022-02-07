f02688abfe4eaa80c6693c38a248f5dc11695aad
Maycon Carlete <maycon.carlete@gmail.com>
2022-02-07 17:56:54 -0500

    test: mock validateCPF function

--- 1-tests/jest-tests/ex3/ex3.spec.js
+++ 1-tests/jest-tests/ex3/ex3.spec.js
@@ -1,4 +1,5 @@
 const validations = require('./ex3')
+const { validateCPF } = require('./ex3')
 
 jest.mock('./ex3', () => ({
   validateEmail: () => false
@@ -19,5 +20,14 @@ describe('should rewrite the module', () => {
     const result = validations.validateEmail(email)
 
     expect(result).toBeTruthy()
+  })
+
+  it('should validate cpf', () => {
+    const mockedFn = jest.fn(validateCPF)
+    mockedFn.mockImplementationOnce(() => false)
+    const cpf = '123.123.123.12'
+    const result = mockedFn(cpf)
+
+    expect(result).toBeFalsy()
   })
 })
