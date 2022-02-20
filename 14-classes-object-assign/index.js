60814945bcbf5e2ea7e3868e5a760aefce575eaa
Maycon Carlete <maycon.carlete@gmail.com>
2022-02-19 20:30:01 -0500

    feat: object.assign and static method

--- 14-classes-object-assign/index.js
+++ 14-classes-object-assign/index.js
@@ -4,11 +4,16 @@ class Calculator {
   }
 }
 
-Object.assign(Calculator.prototype, {
+Object.assign(Calculator.prototype, { // normal method
   sub: (a, b) => a - b
+})
+
+Object.assign(Calculator, { // static method
+  mult:(a, b) => a * b
 })
 
 const calc = new Calculator()
 
 console.log(calc.sum(1, 1))
 console.log(calc.sub(1, 1))
+console.log(Calculator.mult(2,2))
